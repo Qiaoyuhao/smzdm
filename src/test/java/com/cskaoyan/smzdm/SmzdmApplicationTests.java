51e6e083cd730c328ac5b39cb818cf8e625b5903
chris <chris@wangdao.com>
2018-09-11 23:07:38 +0800

    将Springboot版本改为1.3.5.release 将mybatis.spring.boot.start版本改为1.1.1
    基本配置测试

--- src/test/java/com/cskaoyan/smzdm/SmzdmApplicationTests.java
+++ src/test/java/com/cskaoyan/smzdm/SmzdmApplicationTests.java
@@ -2,15 +2,20 @@ package com.cskaoyan.smzdm;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SmzdmApplication.class)
+@WebAppConfiguration
 public class SmzdmApplicationTests {
 
     @Test
     public void contextLoads() {
+        System.out.println("SpringBoot starts!");
     }
 
 }
